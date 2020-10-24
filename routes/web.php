ed9401a5f993a442db4d9565f383969a81fe6b28
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-24 11:32:05 +0900

    [add route to show blogs

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ブログ一覧画面を表示
+Route::get('/', 'BlogController@showList')->name('blogs');
